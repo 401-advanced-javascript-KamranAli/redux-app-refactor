e508c3f4c4b82b8e8a023507084da59b422fba9f
Kamran Ali <kgali21@gmail.com>
2019-11-12 13:26:48 -0800

    first commit + some actions work

---
+++ src/components/actions/MoodActions.js
@@ -0,0 +1,19 @@
+export const DRINK_COFFEE = 'DRINK_COFFEE';
+export const drinkCoffee = () => ({
+  type: DRINK_COFFEE
+});
+
+export const EAT_SNACKS = 'EAT_SNACKS';
+export const eatSnacks = () => ({
+  type: EAT_SNACKS
+});
+
+export const TAKE_NAP = 'TAKE_NAP';
+export const takeNap = () => ({
+  type: TAKE_NAP
+});
+
+export const STUDY = 'STUDY';
+export const studyTime = () => ({
+  type: STUDY
+});
