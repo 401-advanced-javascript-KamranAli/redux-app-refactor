83d1cf64208be839aa39e55ab7f28e8441d6168e
Kamran Ali <kgali21@gmail.com>
2019-11-12 14:33:42 -0800

    actions part of the lab finished

--- src/components/actions/MoodActions.js
+++ src/components/actions/MoodActions.js
@@ -1,19 +1,12 @@
-export const DRINK_COFFEE = 'DRINK_COFFEE';
-export const drinkCoffee = () => ({
-  type: DRINK_COFFEE
+
+export const actions = [
+  { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees' },
+  { name: 'EAT_SNACK', text: 'Snack', stateName: 'snacks' },
+  { name: 'TAKE_NAP', text: 'Nap', stateName: 'naps' },
+  { name: 'STUDY', text: 'Study', stateName: 'studies' },
+];
+
+export const incrementSelection = name => ({
+  type: name
 });
 
-export const EAT_SNACKS = 'EAT_SNACKS';
-export const eatSnacks = () => ({
-  type: EAT_SNACKS
-});
-
-export const TAKE_NAP = 'TAKE_NAP';
-export const takeNap = () => ({
-  type: TAKE_NAP
-});
-
-export const STUDY = 'STUDY';
-export const studyTime = () => ({
-  type: STUDY
-});
--- src/containers/Moods.js
+++ src/containers/Moods.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
-import { drinkCoffee, eatSnacks, takeNap, studyTime } from '../components/actions/MoodActions';
-import { getFace } from '../components/Selectors/Selectors';
+import { incrementSelection, actions } from '../components/actions/MoodActions';
 
-const actions = [
-  { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees' },
-  { name: 'EAT_SNACK', text: 'Snack', stateName: 'snacks' },
-  { name: 'TAKE_NAP', text: 'Nap', stateName: 'naps' },
-  { name: 'STUDY', text: 'Study', stateName: 'studies' },
-];
+export const isTired = state => state.coffees < 1 && state.naps < 1;
+export const isHyper = state => state.coffees > 3;
+export const isEducated = state => state.studies > 2;
+export const isHungry = state => state.snacks < 1;
+
+export const getFace = state => {
+  if(isTired(state) && isHungry(state)) return '🤬';
+  if(isHyper(state) && isHungry(state)) return '🤮';
+  if(isTired(state)) return '😴';
+  if(isHyper(state)) return '🙀';
+  if(isEducated(state)) return '🤯';
+  if(isHungry(state)) return '😡';
+
+  return '😀';
+};
 
 // eslint-disable-next-line react/prop-types
 const Moods = ({ actions, emoji, handleSelection }) => (
@@ -25,18 +33,10 @@ const mapStateToProps = state => ({
   emoji: getFace(state)
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   handleSelection(name) {
-//     dispatch({
-//       type: name
-//     });
-//   }
-// });
-
 const mapDispatchToProps = dispatch => {
   return {
     handleSelection(name) {
-      dispatch(drinkCoffee(name));
+      dispatch(incrementSelection(name));
     }
   };
 };
